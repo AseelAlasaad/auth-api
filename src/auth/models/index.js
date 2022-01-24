a6daf1bd52e55845383b3f5690a7298f7601abbc
Aseel Alasaad <aseelalasaad@gmail.com>
2022-01-24 19:18:54 +0200

    updated

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -3,6 +3,7 @@
 const userModel = require('./users.js');
 const clothesModel = require('./clothes/model.js');
 const foodModel = require('./food/model.js');
+const productsModel=require('./product.js')
 const Collection = require('./data-collection.js');
 
 
@@ -21,10 +22,13 @@ let sequelizeOptions = process.env.NODE_ENV === 'production' ? {
 const sequelize = new Sequelize(DATABASE_URL,sequelizeOptions);
 const food = foodModel(sequelize, DataTypes);
 const clothes = clothesModel(sequelize, DataTypes);
+const product =productsModel(sequelize, DataTypes);
 
 module.exports = {
   db: sequelize,
   users: userModel(sequelize, DataTypes),
   food: new Collection(food),
   clothes: new Collection(clothes),
+  product: new Collection(product),
+
 }
---
+++ src/auth/models/product.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const productsModel = (sequelize, DataTypes) => sequelize.define('Products', {
+  name: { type: DataTypes.STRING, required: true },
+  url: { type: DataTypes.STRING, required: true },
+  category: { type:DataTypes.STRING, required: true},
+  price:{ type:DataTypes.INTEGER, required: true},
+  inStock:{ type:DataTypes.INTEGER, required: true},
+  count:{ type:DataTypes.INTEGER, required: true}
+});
+
+module.exports = productsModel;
